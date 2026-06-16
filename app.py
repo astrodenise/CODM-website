96ca246d687e818ee82f7449517672692680800b
astrodenise <erikadenise79@gmai.com>
2026-06-16 13:58:07 +0800

    add

--- app.py
+++ app.py
@@ -10,5 +10,30 @@ def home():
 def ar():
     return render_template("ar.html")
 
+@app.route("/smg")
+def smg():
+    return render_template("smg.html")
+
+
+@app.route("/sg")
+def sg():
+    return render_template("sg.html")
+
+@app.route("/sr")
+def sr():
+    return render_template("sr.html")
+
+@app.route("/lmg")
+def lmg():
+    return render_template("lmg.html")
+
+@app.route("/mm")
+def mm():
+    return render_template("mm.html")
+
+@app.route("/pistol")
+def pistol():
+    return render_template("pistol.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
